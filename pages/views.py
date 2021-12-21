413d8bee7d7869c05eedc15812e6d1934f3e7f55
Sagar Darekar <sagardarekar1997@gmail.com>
2021-12-21 11:36:28 +0530

    using django templates

--- pages/views.py
+++ pages/views.py
@@ -6,13 +6,16 @@ from django.shortcuts import render
 def home_view(request, *args, **kargs):
     print(request.user);
     print(args, kargs);
-    return HttpResponse("<h1>Hello World</h1>");  #string of HTML code
+    # return HttpResponse("<h1>Hello World</h1>");  #string of HTML code
+    return render(request, 'home.html', {});
 
 def contact_view(request, *args, **kargs):
-    return HttpResponse("<h1>Contact Page</h1>");
+    # return HttpResponse("<h1>Contact Page</h1>");
+    return render(request, 'contact.html', {});
 
 def about_view(request, *args, **kargs):
-    return HttpResponse("<h1>About Page</h1>");
+    # return HttpResponse("<h1>About Page</h1>");
+    return render(request, 'about.html', {});
 
 def social_view(request, *args, **kargs):
     return HttpResponse("<h1>Social Page</h1>");
