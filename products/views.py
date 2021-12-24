a4b00a8d39c05ca74c718183328820bdc9686434
Sagar Darekar <sagardarekar1997@gmail.com>
2021-12-24 12:55:46 +0530

    using conditional statements inside view and displaying data from database using context

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def product_detail_view(req):
+    obj = Product.objects.get(id=1);
+    # context = {
+    #     'title': obj.title,
+    #     'description': obj.description
+    # }
+    context = {
+        'object': obj
+    }
+    return render(req, 'product/detail.html', context);
